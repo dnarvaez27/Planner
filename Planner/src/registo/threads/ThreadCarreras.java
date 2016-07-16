7dad0210f932db88e50cc866f067dfb6e3165ff2
David Narvaez <dnarvaez27@outlook.com>
2016-07-15 22:20:55 -0500

    Upload Java Files

---
+++ Planner/src/registo/threads/ThreadCarreras.java
@@ -0,0 +1,29 @@
+package registo.threads;
+
+import registro.interfaz.InterfazPlanner;
+
+public class ThreadCarreras extends Thread
+{
+	private InterfazPlanner interfaz;
+
+	public ThreadCarreras( InterfazPlanner interfaz )
+	{
+		this.interfaz = interfaz;
+	}
+
+	public void run( )
+	{
+		interfaz.showLoadingCarreras( true );
+
+		try
+		{
+			interfaz.actualizarCarreras( );
+			interfaz.showLoadingCarreras( false );
+		}
+		catch( Exception e )
+		{
+			interfaz.error404( );
+			e.printStackTrace( );
+		}
+	}
+}
