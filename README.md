176fe2b6d2418368ce05234b0e05a1c6d70f3ee5
David Narvaez <dnarvaez27@outlook.com>
2017-02-11 17:51:44 -0500

    Update

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Planner
 _Unofficial schedule planner tool for University of the Andes._
 
-
 ### Implementation
-This program uses HTML to get University of the Andes' Schedule and Subjects information from https://registroapps.uniandes.edu.co/scripts/semestre/adm_con_horario_joomla.php
+This program uses HTML Scraping to get University of the Andes' Schedule and Subjects information from https://registroapps.uniandes.edu.co/scripts/semestre/adm_con_horario_joomla.php
 
 ### Purpose
 * Its main purpose is to enable Students to make its schedule in a comfortable way
-* Soon will be implemented the auto-register subject tool
-
 ____
 **To participate in this project please contact:** [David Narvaez](mailto:dnarvaez27@outlook.com)
--- src/com/dnarvaez27/planner/mundo/Planner.java
+++ src/com/dnarvaez27/planner/mundo/Planner.java
@@ -6,16 +6,16 @@ import java.util.List;
 public class Planner
 {
 	private LinkedList<Materia> materias;
-
+	
 	private Recuperacion recuperacion;
-
+	
 	public Planner( )
 	{
 		recuperacion = new Recuperacion( );
-
+		
 		materias = new LinkedList<>( );
 	}
-
+	
 	public boolean agregarMateria( Materia materia )
 	{
 		if( !materias.contains( materia ) )
@@ -25,19 +25,51 @@ public class Planner
 		}
 		return false;
 	}
-
+	
 	public void eliminarMateria( Materia materia )
 	{
 		materias.remove( materia );
 	}
-
+	
 	public List<Materia> getMaterias( )
 	{
 		return materias;
 	}
-
+	
 	public Recuperacion getRecuperacion( )
 	{
 		return recuperacion;
+	}
+	
+	public static void main( String[ ] args )
+	{
+		Planner pl = new Planner( );
+		
+		try
+		{
+			pl.getRecuperacion( ).iniciarCarreras( );
+			LinkedList<Carrera> c = pl.getRecuperacion( ).getCarreras( );
+			
+			for( Carrera carrera : c )
+			{
+				
+				pl.getRecuperacion( ).getMaterias( carrera );
+				
+				for( Materia m : carrera.getMaterias( ) )
+				{
+					if( m.getSalon( ).equals( ".ML_604" ) )
+					{
+						System.out.println( m.getName( ) + ": " + m .getSalon( ) );
+					}
+				}
+			}
+			
+		}
+		catch( Exception e )
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace( );
+		}
+		
 	}
 }
